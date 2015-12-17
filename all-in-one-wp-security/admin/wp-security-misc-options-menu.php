f21c2295717c74ff43e80b072f8ff58f7b356796
Davide Giunchi <davidegiunchidiennea@users.noreply.github.com>
2015-12-17 11:37:02 +0100

    added tab3
    for users enumeration feature

--- all-in-one-wp-security/admin/wp-security-misc-options-menu.php
+++ all-in-one-wp-security/admin/wp-security-misc-options-menu.php
@@ -10,6 +10,7 @@ class AIOWPSecurity_Misc_Options_Menu extends AIOWPSecurity_Admin_Menu
     var $menu_tabs_handler = array(
         'tab1' => 'render_tab1',
         'tab2' => 'render_tab2',
+        'tab3' => 'render_tab3',
         );
 
     function __construct() 
@@ -168,5 +169,57 @@ class AIOWPSecurity_Misc_Options_Menu extends AIOWPSecurity_Admin_Menu
         </div></div>
         <?php
     }
+    
+    function render_tab3()
+    {
+        global $aio_wp_security;
+        $maint_msg = '';
+        if(isset($_POST['aiowpsec_save_users_enumeration']))
+        {
+            $nonce=$_REQUEST['_wpnonce'];
+            if (!wp_verify_nonce($nonce, 'aiowpsec-users-enumeration'))
+            {
+                $aio_wp_security->debug_logger->log_debug("Nonce check failed on prevent users enumeration feature settings save!",4);
+                die("Nonce check failed on prevent users enumeration frame feature settings save!");
+            }
+
+            //Save settings
+            $aio_wp_security->configs->set_value('aiowps_prevent_users_enumeration',isset($_POST["aiowps_prevent_users_enumeration"])?'1':'');
+            $aio_wp_security->configs->save_config();
+
+            $this->show_msg_updated(__('Users Enumeration Prevention feature settings saved!', 'all-in-one-wp-security-and-firewall'));
+
+        }
+        ?>
+        <div class="postbox">
+        <h3 class="hndle"><label for="title"><?php _e('Prevent Users Enumeration', 'all-in-one-wp-security-and-firewall'); ?></label></h3>
+        <div class="inside">
+        <form action="" method="POST">
+        <?php wp_nonce_field('aiowpsec-users-enumeration'); ?>
+        <div class="aio_blue_box">
+            <?php
+            echo '<p>'.__('This feature allows you to prevent external users/bots from fetching the user info with urls like "/?author=1".', 'all-in-one-wp-security-and-firewall').'</p>';
+            echo '<p>'.__('When enabled, this feature will print "Forbidden" rather than the user informations.', 'all-in-one-wp-security-and-firewall').'</p>';
+            ?>
+        </div>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row"><?php _e('Disable Users Enumeration', 'all-in-one-wp-security-and-firewall')?>:</th>
+                <td>
+                <input name="aiowps_prevent_users_enumeration" type="checkbox"<?php if($aio_wp_security->configs->get_value('aiowps_prevent_users_enumeration')=='1') echo ' checked="checked"'; ?> value="1"/>
+                <span class="description"><?php _e('Check this if you want to stop users enumeration.', 'all-in-one-wp-security-and-firewall'); ?></span>
+                </td>
+            </tr>
+
+        </table>
+
+        <div class="submit">
+            <input type="submit" class="button-primary" name="aiowpsec_save_users_enumeration" value="<?php _e('Save Settings'); ?>" />
+        </div>
+        </form>
+        </div></div>
+        <?php
+    }
+
     
 } //end class
